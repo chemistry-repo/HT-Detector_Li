d4b053b19c158b13eebd38aa277bc4dc0fefb277
hmy-repo <hengmaoyue@gmail.com>
2024-04-25 18:10:23 +0900

    train progress

--- run.py
+++ run.py
@@ -1,16 +1,34 @@
 from ultralytics import YOLO
 
-# 加载模型
-model = YOLO("yolov8n.yaml")  # 从头开始构建新模型
-model = YOLO("weights/yolov8n.pt")  # 加载预训练模型（建议用于训练）
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO('yolov8n.yaml')  # build a new model from YAML
+    model = YOLO('weights/yolo/yolov8n.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('yolov8n.yaml').load('weights/yolo/yolov8n.pt')  # build from YAML and transfer weights
 
-# 使用模型
-# model.train(data="coco128.yaml", epochs=3)  # 训练模型
-# metrics = model.val()  # 在验证集上评估模型性能
-results = model.predict(source="ultralytics/assets/bus.jpg", save=True)  # 对图像进行预测
-# success = model.export(format="onnx")  # 将模型导出为 ONNX 格式
+    # Train the model
+    results = model.train(data='C:/yue/eclipse/MLchem/ImgRec/YOLOv8/custom/data.yaml', epochs=300, device=0)
+    metrics = model.val()  # 在验证集上评估模型性能
 
-# Terminal:
 
-# activate yolov8
-# pip install ultralytics
+
+
+
+
+    #
+    # from ultralytics import YOLO
+    #
+    # # 加载模型
+    # model = YOLO("yolov8n.yaml")  # 从头开始构建新模型
+    # model = YOLO("weights/yolov8n.pt")  # 加载预训练模型（建议用于训练）
+    #
+    # # 使用模型
+    # model.train(data="coco128.yaml", epochs=3)  # 训练模型
+    # metrics = model.val()  # 在验证集上评估模型性能
+    # results = model.predict(source="ultralytics/assets/bus.jpg", save=True)  # 对图像进行预测
+    # success = model.export(format="onnx")  # 将模型导出为 ONNX 格式
+
+    # Terminal:
+
+    # activate yolov8
+    # pip install ultralytics
