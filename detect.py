b50720ce9b837d391cd499da7c2a6c9536fd444c
Hengmao Yue <hengmaoyue@gmail.com>
2024-04-25 18:06:57 +0800

    detect all the image in a folder

--- detect.py
+++ detect.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import os
 
 if __name__ == '__main__':
     # # Load a model
@@ -15,10 +16,20 @@ if __name__ == '__main__':
 
     # # 加载模型
     # model = YOLO("yolov8n.yaml")  # 从头开始构建新模型
-    model = YOLO("weights/mlsensing/yolov8n_640_best.pt")  # 加载预训练模型（建议用于训练）
+    # model = YOLO("weights/mlsensing/yolov8n_640_best.pt")  # 加载预训练模型（建议用于训练）
     #
     # # 使用模型
     # model.train(data="coco128.yaml", epochs=3)  # 训练模型
     # metrics = model.val()  # 在验证集上评估模型性能
-    results = model.predict(source="custom/detectImg/Date0420_S01ml_Num08_Deg000_Dist020cm.jpg", save=True)  # 对图像进行预测
+    # results = model.predict(source="custom/detectImg/Date0420_S01ml_Num08_Deg000_Dist020cm.jpg", save=True)  # 对图像进行预测
     # success = model.export(format="onnx")  # 将模型导出为 ONNX 格式
+
+    # def traverse_files(directory):
+    model = YOLO("weights/mlsensing/yolov8n_640_best.pt")  # 加载预训练模型（建议用于训练）
+    file_names = os.listdir(os.path.join(os.getcwd(), "custom/detectImg/"))
+    abs_file_names = []
+    for file_name in file_names:
+        abs_file_names.append(os.path.join(os.getcwd(), "custom/detectImg/", file_name))
+    for abs_file_name in abs_file_names:
+        results = model.predict(source=abs_file_name, save=True)  # 对图像进行预测
+        print(abs_file_name)
