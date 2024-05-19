28a48968d31830bd441b2aa27fde3bc932a29a2f
Hengmao Yue <hengmaoyue@gmail.com>
2024-05-20 04:11:42 +0800

    test torch.tensor

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ print('cuda_available=', cuda_available)
 print(torch.version.cuda)
 print(torch.cuda.device_count())
 print(torch.cuda.current_device())
+
+tensor = torch.ones((2,), dtype=torch.int8)
+print(tensor)
+mybox = torch.tensor(data=[1, 2, 3, 4], device='cuda:0')
+print(mybox)
