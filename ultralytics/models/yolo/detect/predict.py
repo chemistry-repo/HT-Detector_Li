e7f4cb368088539280c78ba4b6d380c6315fae92
Hengmao Yue <hengmaoyue@gmail.com>
2024-06-23 20:21:06 +0800

    for debug

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -22,6 +22,7 @@ class DetectionPredictor(BasePredictor):
 
     def postprocess(self, preds, img, orig_imgs):
         """Post-processes predictions and returns a list of Results objects."""
+        # print('DetectionPredictor -> def postprocess()......')
         preds = ops.non_max_suppression(
             preds,
             self.args.conf,
