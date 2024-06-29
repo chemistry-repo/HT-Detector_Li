220108bd740b7585e79df3a18f440fe2caef5f3a
Hengmao Yue <hengmaoyue@gmail.com>
2024-06-29 14:21:45 +0800

    seperate linear and detection

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-
+import os, shutil
 import detect
+from interface import linear_dir, detection_dir, results_dir, mode
 
-detect.detection()
+# only delete the results directory at the linear regression stage
+if mode == 'linear':
+    # if path not exists, create
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir) # 父目录缺失会补上，优先创建目录 https://www.geeksforgeeks.org/create-a-directory-in-python/#makedirs
+    # if dir not empty, clean
+    if os.listdir(results_dir):
+        print(os.listdir(results_dir))
+        shutil.rmtree(results_dir)
+        os.makedirs(results_dir)
+    detect.detection(img_dir=linear_dir)
+elif mode == 'detection':
+    detect.detection(img_dir=detection_dir)
+else:
+    raise NameError('Wrong MODE! The mode should be \'linear\' or \'detection\'')
